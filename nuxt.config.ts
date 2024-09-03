069a5f8abfee9216ece37f3b68a22bfda65af71b
Dmitrii Morrows <trollman1993@gmail.com>
2024-09-03 13:46:58 +0600

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: true,
+  ssr: false,
   modules: ['@nuxt/eslint', 'nuxt-quasar-ui', '@pinia/nuxt'],
   css: ['~/assets/styles/main.scss'],
   alias: {
